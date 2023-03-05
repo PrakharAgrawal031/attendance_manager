6387b80118ff0f7b59242b13172b3acf6287a35d
Prakhar <agrawalprakhar031@gmail.com>
2023-03-06 01:27:46 +0530

    create attendance manager

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const e1 = (
+  <>
+    <h1 className="text-xl font-bold text-black">This is a react app</h1>
+    
+  </>
+); 
+root.render(e1);
