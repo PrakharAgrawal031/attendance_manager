337a13ad46aea0201286ded9bb709e365163b81f
Prakhar <agrawalprakhar031@gmail.com>
2023-03-08 22:25:17 +0530

    Update Attendance_Manager

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css"
+
+function App() {
+  
+
+  return (
+    <div className="bg-red-900">
+      <h1 className="font-extrabold bg-red-600  ">Hsfaskdf</h1>
+    </div>
+  )
+}
+
+export default App
