3a0b13387e9ca441a5fe22e17c4a56b9f7d8d36b
Prakhar <agrawalprakhar031@gmail.com>
2023-03-12 02:12:29 +0530

    Create Nav_top.jsx

---
+++ src/components/Nav_top.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Nav_top(){
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const d = new Date();
+const month = months[d.getMonth()];
+    return(
+        <div></div>
+    )
+}
