eb9628d5049b05e5ce9928c5f50222e4a5d79b39
Prakhar <agrawalprakhar031@gmail.com>
2023-03-12 02:12:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './components/App'
+//import Nav_side from './components/Nav_side'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <App />
-  //</React.StrictMode>,
+  </React.StrictMode>,
 )
