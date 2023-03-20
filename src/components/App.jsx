c47ec3b76b793e6ab5f41db494fd5c48c0819347
Prakhar <agrawalprakhar031@gmail.com>
2023-03-20 22:41:07 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Nav_side from "./Nav_side";
-import Nav_top from './Nav_top'
+import Nav_top from './Nav_top';
+import Cards_array from './Card_array';
 function App() {
   return (
     <div className="flex">
       <Nav_side />
-      <div className=" w-[95%] ">
+      <div className=" w-[95%]">
         <Nav_top/>
+        <Cards_array/>
       </div>
     </div>
   );
